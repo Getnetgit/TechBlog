b90f56f2e5c719ab7eeb41f4e7dc3e0a275027a4
Get Ali <getnet4fafi@gmail.com>
2023-05-15 18:29:03 -0700

    code cleanup

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -31,69 +31,6 @@ const displayBlogUpdateForm = async (event) => {
   }
 };
 
-const updateButtonHandler = async (event) => {
-  event.preventDefault();
-  if (event.target.hasAttribute('data-id')) {
-    const id = event.target.getAttribute('data-id');
-    const title = document.querySelector('#title').value.trim();
-    const content = document.querySelector('#content').value.trim();
-    console.log(title)
-    console.log(content)
-    if (title && content && id) {
-      const response = await fetch(`/api/blogs/${id}`, {
-        method: 'PUT',
-        body: JSON.stringify({ title, content }),
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
-
-      if (response.ok) {
-        document.location.replace('/dashboard');
-      } else {
-        alert('Failed to update blog');
-      }
-    }
-  }
-};
-// const delButtonHandler = async (event) => {
-//   if (event.target.hasAttribute('data-id')) {
-//     const id = event.target.getAttribute('data-id');
-
-//     const response = await fetch(`/api/blogs/${id}`, {
-//       method: 'DELETE',
-//     });
-
-//     if (response.ok) {
-//       document.location.replace('/blogs');
-//     } else {
-//       alert('Failed to delete blog');
-//     }
-//   }
-// };
-
-// const blogButtonHandler = async (event) => {
-//   if (event.target.hasAttribute('data-deleteID')) {
-//     const id = event.target.getAttribute('data-id');
-
-//     const response = await fetch(`/api/blogs/${id}`, {
-//       method: 'DELETE',
-//     });
-
-//     if (response.ok) {
-//       document.location.replace('/blogs');
-//     } else {
-//       alert('Failed to delete blog');
-//     }
-//   }
-// };
-
-document.querySelector('#create').addEventListener('click', newFormHandler);
-
-document.querySelector('#Add_New_Blog').addEventListener('click',()=>{
-  document.querySelector('.newBlog').classList.remove('hide')
-})
-
 
 const updateBlogBtnHandler = async (event) => {
   event.preventDefault();
@@ -140,6 +77,13 @@ const deleteBlogBtnHandler = async (event) => {
     }
   }
 };
+
+
+document.querySelector('#create').addEventListener('click', newFormHandler);
+
+document.querySelector('#Add_New_Blog').addEventListener('click',()=>{
+  document.querySelector('.newBlog').classList.remove('hide')
+})
 
 document.querySelector('main').addEventListener('click', displayBlogUpdateForm);
 document.querySelector('main').addEventListener('click', updateBlogBtnHandler);
